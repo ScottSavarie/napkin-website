d180b1f84c5c4de0e187a9a17440001652380130
ScottSavarie <scottsavarie@gmail.com>
2015-02-06 17:50:28 -0800

    new layout changes, make video tappable on mobile

--- napkin/js/main.js
+++
@@ -1,3 +0,0 @@
-/* =Home specialty fade
-============================================================================== */
-$(document).ready(function(){$("#graphic-wrapper").addClass("show-phone"),$("#logo").addClass("show-logo"),$("#intro-text").addClass("show-text"),$("#primary-button").addClass("show-btn"),$("#footer").addClass("show-footer")}),console.log("yoyo");
