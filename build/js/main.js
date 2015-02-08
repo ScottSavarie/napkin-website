04b5c07aec67e9f9dbd3b7f99d80fe2db6e9804e
ScottSavarie <scottsavarie@gmail.com>
2015-02-08 07:42:33 -0800

    add app store icon / layout polish

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,3 @@
 /* =Home specialty fade
 ============================================================================== */
-$(document).ready(function(){$("#graphic-wrapper").addClass("show-phone"),$("#logo").addClass("show-logo"),$("#intro-text").addClass("show-text"),$("#primary-button").addClass("show-btn"),$("#footer").addClass("show-footer")}),console.log("yoyo");
+$(document).ready(function(){$("#graphic-wrapper").addClass("show-phone"),$("#logo").addClass("show-logo"),$("#intro-text").addClass("show-text"),$("#primary-button").addClass("show-btn")}),console.log("yoyo");
