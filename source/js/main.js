1cc47cc7232855bb545418bf4dec8dece637ba42
Scott Savarie <scottsavarie@gmail.com>
2015-06-26 16:29:12 +0200

    add id

--- source/js/main.js
+++ source/js/main.js
@@ -6,9 +6,7 @@ $(document).ready(function() {
   $("#graphic-wrapper").addClass("show-phone");  
   $("#logo").addClass("show-logo");
   $("#intro-text").addClass("show-text");
-  $("#primary-button").addClass("show-btn");
-
-    
+  $("#primary-button").addClass("show-btn");    
 });
 
 console.log("yoyo");
